845d3bdc217e350832394bcf736291c208694cb9
Justin Movitz <movitzj1@montclair.edu>
2020-10-24 06:42:28 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -20,6 +20,7 @@ def literal_aoh
     {name: "Pat Monteseian", occupation: "Staff"},
     {name:  "Kate Gompert", occupation: "None"},
     {name:  "Bruce Green", occupation: "Fan of Mildred"}
+  ]
 
 
   # Using Array literal syntax only, build a nested array that uses the data in
